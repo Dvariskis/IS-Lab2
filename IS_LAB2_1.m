3f9bf4d2b0b0768d4fb5f935c6b917cc7dfbdecd
Dvariskis <91151425+Dvariskis@users.noreply.github.com>
2021-12-09 22:30:55 +0200

    Add files via upload

--- IS_LAB2_1.m
+++ IS_LAB2_1.m
@@ -34,8 +34,8 @@ w15_2 = rand(1)*0.1;
 w16_2 = rand(1)*0.1;
 b1_2 = rand(1)*0.1;
 
-n = 0.15; %Mokymo žingsnis
-for i = 1:10000 %Tinklo apmokymo pakartojimai
+n = 0.1; %Mokymo žingsnis
+for i = 1:100000 %Tinklo apmokymo pakartojimai
     for i = 1:length(x)
         %3. Skaičuojama tinklo atsaka
         %3.1 Pirmojo sluoksnio neuronams
@@ -46,29 +46,29 @@ for i = 1:10000 %Tinklo apmokymo pakartojimai
         v5_1 = x(i)*w51_1 + b5_1;
         v6_1 = x(i)*w61_1 + b6_1;
         % Aktyvavimo funkcijos pritaikymas
-        y1_1 = 1/(1+exp(-v1_1));
-        y2_1 = 1/(1+exp(-v2_1));
-        y3_1 = 1/(1+exp(-v3_1));
-        y4_1 = 1/(1+exp(-v4_1));
-        y5_1 = 1/(1+exp(-v5_1));
-        y6_1 = 1/(1+exp(-v6_1));
+        y1_1 = tanh(v1_1);
+        y2_1 = tanh(v2_1);
+        y3_1 = tanh(v3_1);
+        y4_1 = tanh(v4_1);
+        y5_1 = tanh(v5_1);
+        y6_1 = tanh(v6_1);
         %3.2 Antrojo sluoksnio neuronui
         v1_2 = y1_1*w11_2 + y2_1*w12_2 + y3_1*w13_2 + y4_1*w14_2 + y5_1*w15_2 + y6_1*w16_2;
         % Aktyvavimo funkcijos pritaikymas išėjimo neuronui
-        y1_2 = tanh(v1_2);
+        y1_2 = v1_2;
         y = y1_2;
         %3.3 Skaičiuojama klaidą
         e = d(i) - y;
         %4 Ryšių svorių atnaujinimas
         %4.1 Skaičiuojama klaidos gradientą išėjimo sluoksnio neuronui
-        delta1_2 = (1 - (tanh(v1_2))^2)*e; %Aktyvavimo funkcijos išvestinė dauginta iš momentinės klaidos
+        delta1_2 = e;
         %4.2 Skaičiuojama klaidos gradientą paslėptojo sluoksnio neuronams
-        delta1_1 = y1_1*(1 - y1_1)*delta1_2*w11_2; %Aktyvavimo f-jos išvestinė dauginta iš antrojo sluoksnio delta
-        delta2_1 = y2_1*(1 - y2_1)*delta1_2*w12_2;
-        delta3_1 = y3_1*(1 - y3_1)*delta1_2*w13_2;
-        delta4_1 = y4_1*(1 - y4_1)*delta1_2*w14_2;
-        delta5_1 = y5_1*(1 - y5_1)*delta1_2*w15_2;
-        delta6_1 = y6_1*(1 - y6_1)*delta1_2*w16_2;
+        delta1_1 = (1-(tanh(y1_1))^2)*delta1_2*w11_2;   %Aktyvavimo f-jos išvestinė dauginta iš antrojo sluoksnio delta
+        delta2_1 = (1-(tanh(y2_1))^2)*delta1_2*w12_2;
+        delta3_1 = (1-(tanh(y3_1))^2)*delta1_2*w13_2;
+        delta4_1 = (1-(tanh(y4_1))^2)*delta1_2*w14_2;
+        delta5_1 = (1-(tanh(y5_1))^2)*delta1_2*w15_2;
+        delta6_1 = (1-(tanh(y6_1))^2)*delta1_2*w16_2;
         %4.3 Atnaujinamas išėjimo sluoksnio svoriai
         w11_2 = w11_2 + n.*delta1_2*y1_1;
         w12_2 = w12_2 + n.*delta1_2*y2_1;
@@ -94,27 +94,28 @@ for i = 1:10000 %Tinklo apmokymo pakartojimai
 end
 
 %Gaunamos tinklo reikšmės po apmokymo
+X = [0.1:1/60:1];
 Y = zeros(1,length(d));
-for i = 1:length(x)
+for i = 1:length(X)
         %3. Skaičuojama tinklo atsaka
         %3.1 Pirmojo sluoksnio neuronams
-        v1_1 = x(i)*w11_1 + b1_1;
-        v2_1 = x(i)*w21_1 + b2_1;
-        v3_1 = x(i)*w31_1 + b3_1;
-        v4_1 = x(i)*w41_1 + b4_1;
-        v5_1 = x(i)*w51_1 + b5_1;
-        v6_1 = x(i)*w61_1 + b6_1;
+        v1_1 = X(i)*w11_1 + b1_1;
+        v2_1 = X(i)*w21_1 + b2_1;
+        v3_1 = X(i)*w31_1 + b3_1;
+        v4_1 = X(i)*w41_1 + b4_1;
+        v5_1 = X(i)*w51_1 + b5_1;
+        v6_1 = X(i)*w61_1 + b6_1;
         % aktyvavimo f-jos pritaikymas
-        y1_1 = 1/(1+exp(-v1_1));
-        y2_1 = 1/(1+exp(-v2_1));
-        y3_1 = 1/(1+exp(-v3_1));
-        y4_1 = 1/(1+exp(-v4_1));
-        y5_1 = 1/(1+exp(-v5_1));
-        y6_1 = 1/(1+exp(-v6_1));
+        y1_1 = tanh(v1_1);
+        y2_1 = tanh(v2_1);
+        y3_1 = tanh(v3_1);
+        y4_1 = tanh(v4_1);
+        y5_1 = tanh(v5_1);
+        y6_1 = tanh(v6_1);
         %3.2 Antrojo sluoksnio neuronai
         v1_2 = y1_1*w11_2 + y2_1*w12_2 + y3_1*w13_2 + y4_1*w14_2 + y5_1*w15_2 + y6_1*w16_2;
         % aktyvavimo f-jos pritaikymas
-        y1_2 = tanh(v1_2);
+        y1_2 = v1_2;
         y = y1_2;
         %3.3 Saugome tarpines reikšmes
         Y(i) = y;
@@ -122,7 +123,7 @@ for i = 1:length(x)
 end
 
 hold on 
-plot(x,Y,'r+')
+plot(X,Y,'r+')
 hold off;
         
         
--- IS_LAB2_2.m
+++ IS_LAB2_2.m
@@ -5,7 +5,8 @@
 
 %1. Duomenų paruošimas
 x1 = [0.1:1/22:1];
-x2 = [0.1:1/22:1];
+x2 = [1:1/22:1.9];
+%mesh grid
 d = (1 + 0.6*sin(2*pi*x1/0.7) + 0.3*sin(2*pi*x2))./2;
 figure(1);
 plot(x1,d,'b*')
@@ -39,8 +40,8 @@ w15_2 = rand(1)*0.1;
 w16_2 = rand(1)*0.1;
 b1_2 = rand(1)*0.1;
 
-n = 0.15; %Mokymo žingsnis
-for i_n = 1:10000 %Tinklo apmokymo pakartojimai
+n = 0.1; %Mokymo žingsnis
+for i_n = 1:100000 %Tinklo apmokymo pakartojimai
     for i = 1:length(x1)
         %3. Skaičuojama tinklo atsaka
         %3.1 Pirmojo sluoksnio neuronams
@@ -51,29 +52,29 @@ for i_n = 1:10000 %Tinklo apmokymo pakartojimai
         v5_1 = x1(i)*w51_1 + x2(i)*w52_1 + b5_1;
         v6_1 = x1(i)*w61_1 + x2(i)*w62_1 + b6_1;
         % Atyvavimo funkcijos pritaikymas
-        y1_1 = 1/(1+exp(-v1_1));
-        y2_1 = 1/(1+exp(-v2_1));
-        y3_1 = 1/(1+exp(-v3_1));
-        y4_1 = 1/(1+exp(-v4_1));
-        y5_1 = 1/(1+exp(-v5_1));
-        y6_1 = 1/(1+exp(-v6_1));
+        y1_1 = tanh(v1_1);
+        y2_1 = tanh(v2_1);
+        y3_1 = tanh(v3_1);
+        y4_1 = tanh(v4_1);
+        y5_1 = tanh(v5_1);
+        y6_1 = tanh(v6_1);
         %3.2 Antrojo sluoksnio neuronui
         v1_2 = y1_1*w11_2 + y2_1*w12_2 + y3_1*w13_2 + y4_1*w14_2 + y5_1*w15_2 + y6_1*w16_2;
         % Aktyvavimo funkcijos pritaikymas
-        y1_2 = tanh(v1_2);
+        y1_2 = v1_2;
         y = y1_2;
         %3.3 Skaičiuojama klaidą
         e = d(i) - y;
         %4 Ryšių svorių atnaujinimas
         %4.1 Skaičiuojama klaidos gradientą išėjimo sluoksnio neuronui
-        delta1_2 = (1 - (tanh(v1_2))^2)*e;
+        delta1_2 = e;
         %4.2 Skaičiuojama klaidos gradientą paslėptojo sluoksnio neuronams
-        delta1_1 = y1_1*(1 - y1_1)*delta1_2*w11_2;
-        delta2_1 = y2_1*(1 - y2_1)*delta1_2*w12_2;
-        delta3_1 = y3_1*(1 - y3_1)*delta1_2*w13_2;
-        delta4_1 = y4_1*(1 - y4_1)*delta1_2*w14_2;
-        delta5_1 = y5_1*(1 - y5_1)*delta1_2*w15_2;
-        delta6_1 = y6_1*(1 - y6_1)*delta1_2*w16_2;
+        delta1_1 = (1-(tanh(y1_1))^2)*delta1_2*w11_2;   %Aktyvavimo f-jos išvestinė dauginta iš antrojo sluoksnio delta
+        delta2_1 = (1-(tanh(y2_1))^2)*delta1_2*w12_2;
+        delta3_1 = (1-(tanh(y3_1))^2)*delta1_2*w13_2;
+        delta4_1 = (1-(tanh(y4_1))^2)*delta1_2*w14_2;
+        delta5_1 = (1-(tanh(y5_1))^2)*delta1_2*w15_2;
+        delta6_1 = (1-(tanh(y6_1))^2)*delta1_2*w16_2;
         %4.3 Atnaujinamas išėjimo sluoksnio svoriai
         w11_2 = w11_2 + n.*delta1_2*y1_1;
         w12_2 = w12_2 + n.*delta1_2*y2_1;
@@ -111,26 +112,28 @@ end
 
 %Gaunamos tinklo reikšmės po apmokymo
 Y = zeros(1,length(d));
-for i = 1:length(x1)
+X1 = [0.1:1/60:1];
+X2 = [1:1/60:1.9];
+for i = 1:length(X1)
         %3. Skaičuojama tinklo atsaka
         %3.1 Pirmojo sluoksnio neuronams
-        v1_1 = x1(i)*w11_1 + x2(i)*w12_1 + b1_1;
-        v2_1 = x1(i)*w21_1 + x2(i)*w22_1 + b2_1;
-        v3_1 = x1(i)*w31_1 + x2(i)*w32_1 + b3_1;
-        v4_1 = x1(i)*w41_1 + x2(i)*w42_1 + b4_1;
-        v5_1 = x1(i)*w51_1 + x2(i)*w52_1 + b5_1;
-        v6_1 = x1(i)*w61_1 + x2(i)*w62_1 + b6_1;
+        v1_1 = X1(i)*w11_1 + X2(i)*w12_1 + b1_1;
+        v2_1 = X1(i)*w21_1 + X2(i)*w22_1 + b2_1;
+        v3_1 = X1(i)*w31_1 + X2(i)*w32_1 + b3_1;
+        v4_1 = X1(i)*w41_1 + X2(i)*w42_1 + b4_1;
+        v5_1 = X1(i)*w51_1 + X2(i)*w52_1 + b5_1;
+        v6_1 = X1(i)*w61_1 + X2(i)*w62_1 + b6_1;
         % aktyvavimo f-jos pritaikymas
-        y1_1 = 1/(1+exp(-v1_1));
-        y2_1 = 1/(1+exp(-v2_1));
-        y3_1 = 1/(1+exp(-v3_1));
-        y4_1 = 1/(1+exp(-v4_1));
-        y5_1 = 1/(1+exp(-v5_1));
-        y6_1 = 1/(1+exp(-v6_1));
+        y1_1 = tanh(v1_1);
+        y2_1 = tanh(v2_1);
+        y3_1 = tanh(v3_1);
+        y4_1 = tanh(v4_1);
+        y5_1 = tanh(v5_1);
+        y6_1 = tanh(v6_1);
         %3.2 Antrojo sluoksnio neuronai
         v1_2 = y1_1*w11_2 + y2_1*w12_2 + y3_1*w13_2 + y4_1*w14_2 + y5_1*w15_2 + y6_1*w16_2;
         % aktyvavimo f-jos pritaikymas
-        y1_2 = tanh(v1_2);
+        y1_2 = v1_2;
         y = y1_2;
         %3.3 Saugome tarpines reikšmes
         Y(i) = y;
@@ -138,5 +141,5 @@ for i = 1:length(x1)
 end
 
 hold on 
-plot(x1,Y,'r+')
+plot(X1,Y,'r+')
 hold off;
